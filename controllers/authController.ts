ee05362e24737a5cc08b7cd1b22bf22cce401a9a
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2022-08-12 22:18:58 +0200

    Create controller for all auth related functionality

---
+++ controllers/authController.ts
@@ -0,0 +1,89 @@
+import User from '../models/user.js';
+import UserSerializer from "../serializers/userSerializer.js";
+
+
+/**
+ * Create a new user if a user with the same email address or username doesn't
+ * already exist and all data is valid.
+ * @param {Object} req    The request object
+ * @param {Object} res    The response object
+ * @param {Function} next The next middleware function
+ * @returns {Promise<*>}
+ */
+const register = async (req, res, next) => {
+    const serializer = new UserSerializer(req.body);
+
+    if (!serializer.isValid({})) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Invalid data',
+            errors: serializer.errors
+        });
+    }
+
+    const user = await User.findOne({ $or: [{ email: req.data.email }, { username: req.data.username }] });
+    if (user) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'User with this email already exists'
+        });
+
+    } else {
+        const newUser = await serializer.save();
+        const token = newUser.createJWT();
+        res.status(201).json({
+            status: 'success',
+            user: serializer.data(),
+            token
+        });
+    }
+}
+
+/**
+ * Get the user with the given id.
+ * @param {Object} req    The request object
+ * @param {Object} res    The response object
+ * @returns {Promise<void>}
+ */
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const serializer = new UserSerializer(req.body);
+
+    if (!serializer.isValid({skipRequired: "name"})) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Invalid data',
+            errors: serializer.errors
+        });
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'User with this email does not exist',
+        });
+    }
+
+    const isValidPassword = await user.verifyPassword(password);
+    if (!isValidPassword) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Invalid password',
+            user: serializer.data()
+        });
+    }
+
+    const token = user.createJWT();
+    res.status(200).json({
+        status: 'success',
+        user: serializer.data(),
+        token
+    });
+}
+
+const updateUser = async (req, res) => {
+    res.send('updateUser user')
+}
+
+export { register, login, updateUser }
