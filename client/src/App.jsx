318953bf08a7afa6d4318c08e44e85dfdedcd00e
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2022-11-29 22:48:22 +0200

    Create basic dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Register, Login, Error } from './pages';
+import { Register, Login, Error, Dashboard } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/register" element={ <Register/> } />
         <Route path="/login" element={ <Login/> } />
         <Route path="*" element={ <Error/> } />
