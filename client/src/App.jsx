c125ceea7585ac59066a0c16189489b7dee2eb43
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2022-12-02 22:32:48 +0200

    Make the dashboard a protected page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Register, Login, Error, Dashboard } from './pages';
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={ <ProtectedRoute><Dashboard /></ProtectedRoute> } >
+        </Route>
         <Route path="/register" element={ <Register/> } />
         <Route path="/login" element={ <Login/> } />
         <Route path="*" element={ <Error/> } />
