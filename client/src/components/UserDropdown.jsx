37aa0a91880981e682b782b88696ff0865e144b6
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2023-01-08 00:23:24 +0200

    Create component for the dropdown menu for the user related menu options

---
+++ client/src/components/UserDropdown.jsx
@@ -0,0 +1,83 @@
+import { Menu, Transition } from '@headlessui/react';
+import { Fragment } from 'react';
+
+
+const UserDropdown = (options) => {
+    const { user, logout } = options;
+
+    return (
+        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {/* Profile dropdown */}
+            <Menu as="div" className="relative ml-3">
+                <div>
+                    <Menu.Button className="flex rounded-full bg-gray-200 text-sm focus:outline-none focus:ring-2
+                                            focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300">
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                            className="h-8 w-8 rounded-full"
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1
+                                 .2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt=""
+                        />
+                    </Menu.Button>
+                </div>
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md
+                                           bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5
+                                           focus:outline-none divide-y divide-gray-100 rounded shadow">
+                        <div className="px-4 py-3">
+                            <span className="block text-sm text-gray-900 dark:text-white">{ user.username }</span>
+                            <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+                                { user.email }
+                            </span>
+                        </div>
+                        <div>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <a href="#"
+                                        className={
+                                            `${active ? 'bg-gray-100' : ''} block px-4 py-2 text-sm text-gray-700`
+                                        }
+                                    >
+                                        Your Profile
+                                    </a>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <a href="#"
+                                        className = {
+                                            `${active ? 'bg-gray-100' : ''} block px-4 py-2 text-sm text-gray-700`
+                                        }
+                                    >
+                                        Settings
+                                    </a>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <span className={
+                                        `${active ? 'bg-gray-100' : ''} block px-4 py-2 text-sm text-gray-700`
+                                    }
+                                    onClick={ logout }>
+                                        Sign out
+                                    </span>
+                                )}
+                            </Menu.Item>
+                        </div>
+                    </Menu.Items>
+                </Transition>
+            </Menu>
+        </div>
+    );
+};
+
+export default UserDropdown;
