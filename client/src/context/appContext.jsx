1d08659aaa157ca5d973ffe054bf0014320b4497
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2023-01-08 11:15:09 +0200

    Format appContext to eslint specs

--- client/src/context/appContext.jsx
+++ client/src/context/appContext.jsx
@@ -37,7 +37,8 @@ const initialState = {
 
 const AppContext = React.createContext(initialState);
 
-const AppContext = React.createContext(initialState)
+const AppProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     // If user is none, get user from server
     useEffect(() => {
@@ -53,91 +54,90 @@ const AppContext = React.createContext(initialState)
         }
     }, []);
 
-    const displayAlert = (text: string, alertType: string) => {
+    const displayAlert = (text, alertType) => {
         if (!alertType) {
-            alertType = 'danger'
+            alertType = 'danger';
         }
 
-        dispatch({ type: Actions.DISPLAY_ALERT, payload: {text, alertType} })
-    }
+        dispatch({ type: Actions.DISPLAY_ALERT, payload: { text, alertType } });
+    };
 
     const clearAlert = () => {
-        dispatch({ type: Actions.CLEAR_ALERT })
-    }
+        dispatch({ type: Actions.CLEAR_ALERT });
+    };
 
     const addUserToLocalStorage = ({ user }) => {
-        localStorage.setItem('user', JSON.stringify(user))
-    }
+        localStorage.setItem('user', JSON.stringify(user));
+    };
 
     const removeUserFromLocalStorage = () => {
-        localStorage.removeItem('user')
-    }
+        localStorage.removeItem('user');
+    };
 
     const registerUser = async (userDetails) => {
-        dispatch({ type: Actions.REGISTER_USER_BEGIN })
+        dispatch({ type: Actions.REGISTER_USER_BEGIN });
 
         try {
-            const response = await axios.post('/api/auth/register', userDetails)
+            const response = await axios.post('/api/auth/register', userDetails);
             const { user } = response.data;
-            dispatch({ type: Actions.REGISTER_USER_SUCCESS, payload: { user } })
-            addUserToLocalStorage({ user })
+            dispatch({ type: Actions.REGISTER_USER_SUCCESS, payload: { user } });
+            addUserToLocalStorage({ user });
         } catch (error) {
-            console.log(error)
+            console.log(error);
             dispatch({
                 type: Actions.REGISTER_USER_ERROR,
                 payload: {
                     message: error.response.data.message,
                     status: error.response.data.status,
-                    errors: error.response.data.errors
-                }
-            })
+                    errors: error.response.data.errors,
+                },
+            });
         }
-    }
+    };
 
     const loginUser = async (userDetails) => {
-        dispatch({ type: Actions.LOGIN_USER_BEGIN })
+        dispatch({ type: Actions.LOGIN_USER_BEGIN });
 
         try {
-            const response = await axios.post('/api/auth/login', userDetails)
+            const response = await axios.post('/api/auth/login', userDetails);
             const { user } = response.data;
-            dispatch({ type: Actions.LOGIN_USER_SUCCESS, payload: { user } })
-            addUserToLocalStorage({ user })
+            dispatch({ type: Actions.LOGIN_USER_SUCCESS, payload: { user } });
+            addUserToLocalStorage({ user });
             // navigate to dashboard
-            window.location.href = '/'
-
+            window.location.href = '/';
         } catch (error) {
-            console.log(error)
+            console.log(error);
             dispatch({
                 type: Actions.LOGIN_USER_ERROR,
                 payload: {
                     message: error.response.data.message,
                     status: error.response.data.status,
-                    errors: error.response.data.errors
-                }
-            })
+                    errors: error.response.data.errors,
+                },
+            });
         }
-    }
+    };
 
     const logoutUser = async () => {
-        removeUserFromLocalStorage()
+        removeUserFromLocalStorage();
         // Delete the `connect.sid` cookie
-        document.cookie = 'connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
-        const response = await axios.get('/api/auth/logout')
+        document.cookie = 'connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        const response = await axios.get('/api/auth/logout');
         if (response.status === 200) {
-            dispatch({ type: Actions.LOGOUT_USER })
+            dispatch({ type: Actions.LOGOUT_USER });
             window.location.reload();
         }
-    }
+    };
 
     return (
         <AppContext.Provider value={{ ...state, displayAlert, clearAlert, registerUser, loginUser, logoutUser }}>
-        { children }
+            { children }
         </AppContext.Provider>
-    )
-}
-
-const useAppContext = () => {
-    return useContext(AppContext)
+    );
 };
 
-export { AppProvider, initialState, useAppContext }
+const useAppContext = () => {
+    return useContext(AppContext);
+};
+
+export { AppProvider, initialState, useAppContext };
