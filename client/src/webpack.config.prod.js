669aeada37d36baa064bfd16b410eb5a3f9a4a77
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2023-02-03 17:57:45 +0200

    Create webpack config for prod

---
+++ client/src/webpack.config.prod.js
@@ -0,0 +1,67 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InterpolateHtmlPlugin = require('interpolate-html-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[fullhash].js',
+        publicPath: '/',
+    },
+    devServer: {
+        port: 3000,
+        hot: true,
+        open: true,
+        historyApiFallback: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                router: () => 'http://localhost:5050',
+            },
+            'changeOrigin': true,
+        },
+    },
+    resolve: {
+        extensions: ['.jsx', '.js', '.json', '.css'],
+    },
+    mode: 'production',
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new InterpolateHtmlPlugin({
+            PUBLIC_URL: '', // can modify `static` to another name or get it from `process`
+        }),
+    ],
+};
