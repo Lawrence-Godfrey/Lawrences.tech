352d9dce4d66f13d58c7651c3c29b2906fd35b2f
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2023-02-19 20:38:44 +0200

    Allow trailing lines to be toggled on or off

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -9,6 +9,7 @@ const Dashboard = () => {
         sphereRadius: 0.5,
         timeStep: 0.001,
         startRange: 1,
+        linesOn: true,
     });
 
     const setSphereRadius = (e) => {
@@ -25,6 +26,10 @@ const Dashboard = () => {
 
     const setNumAgents = (e) => {
         setSimulationValues({ ...simulationValues, numAgents: e.target.value });
+    };
+
+    const toggleLines = () => {
+        setSimulationValues({ ...simulationValues, linesOn: !simulationValues.linesOn });
     };
 
     return (
@@ -48,7 +53,9 @@ const Dashboard = () => {
                     <LorenzAttractor numAgents={simulationValues.numAgents}
                         sphereRadius={simulationValues.sphereRadius}
                         timeStep={simulationValues.timeStep}
-                        startRange={simulationValues.startRange} />
+                        startRange={simulationValues.startRange}
+                        linesOn={simulationValues.linesOn}
+                    />
                     <style>
                         {`
                             @media (min-width: 800px) {
@@ -62,37 +69,67 @@ const Dashboard = () => {
                         `}
                     </style>
                 </div>
-                <div className="relative h-full w-1/4 absolute z-10 w-auto grid-cols-2 text-sm bg-white
-                border border-gray-100 rounded-lg shadow-md dark:border-gray-700 md:grid-cols-3 dark:bg-gray-700">
+                <div className="relative h-full w-1/4 absolute z-10 w-auto grid-cols-1 text-sm bg-white
+                border border-gray-100 rounded-lg shadow-md dark:border-gray-700 md:grid-cols-2 dark:bg-gray-700">
                     <div className="p-6">
                         <h3 className="text-xl font-bold mb-4">Toolbar</h3>
                         <label htmlFor="default-range"
-                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            className="block mb-1 mt-1 text-sm font-medium text-gray-900 dark:text-white">
+                            Lines
+                        </label>
+                        <div className="flex justify-center">
+                            <label className="relative inline-flex items-center cursor-pointer justify-center mb-4">
+                                <input type="checkbox" value="" className="sr-only peer" onClick={toggleLines}
+                                    checked={simulationValues.linesOn} ></input>
+                                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4
+                                    peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer
+                                    dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white
+                                    after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white
+                                    after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5
+                                    after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                                </div>
+                            </label>
+                        </div>
+                        <hr className="w-48 h-px mx-auto my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                        <label htmlFor="default-range"
+                            className="block mb-1 mt-1 text-sm font-medium text-gray-900 dark:text-white">
                             Sphere Radius
                         </label>
-                        <RangeSlider value={simulationValues.sphereRadius}
-                            onChange={setSphereRadius} min={0.1} max={1} step={0.1} />
+                        <div className="flex justify-center">
+                            <RangeSlider value={simulationValues.sphereRadius}
+                                onChange={setSphereRadius} min={0.1} max={1} step={0.1} />
+                        </div>
+                        <hr className="w-48 h-px mx-auto my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
                         <label htmlFor="default-range"
-                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            className="block mb-1 mt-1 text-sm font-medium text-gray-900 dark:text-white">
                             Start Range
                         </label>
-                        <RangeSlider value={simulationValues.startRange}
-                            onChange={setStartRange} min={0.1} max={1} step={0.1} />
+                        <div className="flex justify-center">
+                            <RangeSlider value={simulationValues.startRange}
+                                onChange={setStartRange} min={0.1} max={1} step={0.1} />
+                        </div>
+                        <hr className="w-48 h-px mx-auto my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
                         <label htmlFor="default-range"
-                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            className="block mb-1 mt-1 text-sm font-medium text-gray-900 dark:text-white">
                             Time Step
                         </label>
-                        <RangeSlider value={simulationValues.timeStep}
-                            onChange={setTimeStep} min={0.0005} max={0.01} step={0.001} />
+                        <div className="flex justify-center">
+                            <RangeSlider value={simulationValues.timeStep}
+                                onChange={setTimeStep} min={0.0005} max={0.01} step={0.001} />
+                        </div>
+                        <hr className="w-48 h-px mx-auto my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
                         <label htmlFor="default-range"
-                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            className="block mb-1 mt-1 text-sm font-medium text-gray-900 dark:text-white">
                             Number of Spheres
                         </label>
-                        <RangeSlider value={simulationValues.numAgents}
-                            onChange={setNumAgents} min={1} max={20} step={1} />
+                        <div className="flex justify-center">
+                            <RangeSlider value={simulationValues.numAgents}
+                                onChange={setNumAgents} min={1} max={20} step={1} />
+                        </div>
                     </div>
                 </div>
             </div>
+            <br></br>
         </div>
     );
 };
