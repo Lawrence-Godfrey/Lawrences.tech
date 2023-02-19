e4e956e339408077f59e2a0875d1385821b549c5
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2023-02-19 15:47:35 +0200

    Create range slider component

---
+++ client/src/components/RangeSlider.jsx
@@ -0,0 +1,18 @@
+
+
+const RangeSlider = ({ value, onChange, min, max, step }) => {
+    return (
+        <div className="relative flex items-center">
+            <input id="default-range" type="range" min={min} max={max} step={step} value={value}
+                onChange={onChange}
+                className="w-32 h-2 bg-gray-200 rounded-lg appearance-none
+                                   cursor-pointer dark:bg-gray-700" />
+            <div className="text-xl mt-4 mt-4" style={{ marginLeft: '15px', marginBottom: '17px' }}>
+                {value}
+            </div>
+        </div>
+    );
+};
+
+
+export default RangeSlider;
