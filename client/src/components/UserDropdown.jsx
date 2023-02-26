3ddf32d840ebe8a163f13f6d9c94b54a2347c0d3
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2023-02-26 22:24:14 +0200

    Fix crossorigin issues with images

--- client/src/components/UserDropdown.jsx
+++ client/src/components/UserDropdown.jsx
@@ -31,6 +31,7 @@ const UserDropdown = () => {
                             src={user.avatar ? user.avatar : '' +
                                 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y'}
                             referrerPolicy="no-referrer"
+                            crossOrigin="anonymous"
                             alt=""
                         />
                     </Menu.Button>
