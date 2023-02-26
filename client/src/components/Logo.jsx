2c8f6bbbcdeec1708497f16dc09a2b8834ac8638
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2023-02-26 22:24:48 +0200

    Add logo and remove unused image files

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,4 +1,3 @@
-import logo from '../assets/images/logo.svg';
 import React from 'react';
 
 const Logo = () => {
@@ -6,7 +5,6 @@ const Logo = () => {
         <a href="/" className="flex items-center justify-center mb-6 text-2xl
              font-semibold text-gray-900 dark:text-white mr-6">
             <img className="w-8 h-8 mr-2"
-                src={logo}
                 alt="logo"/>
             Lawrences.tech
         </a>
