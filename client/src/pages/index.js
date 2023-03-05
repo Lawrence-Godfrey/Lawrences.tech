6b6214b68f425bf2fefa5ae2b00bb8c673e203d6
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2023-03-05 16:46:55 +0200

    Add privacy policy page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,5 +3,6 @@ import Register from './Register';
 import Login from './Login';
 import Dashboard from './Dashboard';
 import Profile from './Profile';
+import PrivacyPolicy from './PrivacyPolicy';
 
-export { Error, Register, Login, Dashboard, Profile };
+export { Error, Register, Login, Dashboard, Profile, PrivacyPolicy };
