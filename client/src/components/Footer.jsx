e96a6051c39b996807a7f1bf38e63ffebb730048
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2023-06-24 19:54:38 +0200

    Add basic card layout project page with placeholder data.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
     return (
         <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between
-        md:p-6 dark:bg-gray-800"
+        md:p-6 dark:bg-gray-800 mt-auto"
         >
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                 © 2023
