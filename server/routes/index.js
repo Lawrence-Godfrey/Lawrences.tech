7d62a645265b64bcb94e7ed0ba8f4de0ed32643c
Lawrence Godfrey <lawrencegodfrey58@gmail.com>
2023-07-23 22:17:52 +0200

    Add routes and handlers for articles API.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 import authRouter from './authRoutes.js';
 import userRouter from './userRoutes.js';
+import articleRouter from './articleRoutes.js';
+
 
 export {
     authRouter,
-    userRouter
+    userRouter,
+    articleRouter,
 }
